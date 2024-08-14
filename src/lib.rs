e0431c5bb6b14eb3a85bb0d7ab499283af1f1168
Jacobtread <jacobtread@gmail.com>
2024-08-14 23:01:40 +1200

    feat: added list of convertable mime types

--- src/lib.rs
+++ src/lib.rs
@@ -137,3 +137,63 @@ pub async fn start_unoserver() -> Result<(), OfficeError> {
     sleep(Duration::from_secs(5)).await;
     Ok(())
 }
+
+/// Checks if the provided mime is included in the known convertable mime types
+pub fn is_known_convertable(mime: &str) -> bool {
+    CONVERTABLE_FORMATS.contains(&mime)
+}
+
+/// List of supported convertable formats
+const CONVERTABLE_FORMATS: &[&str] = &[
+    "text/html",
+    "application/msword",
+    "application/vnd.oasis.opendocument.text-flat-xml",
+    "application/rtf",
+    "application/vnd.sun.xml.writer",
+    "application/vnd.wordperfect",
+    "application/vnd.ms-works",
+    "application/x-mswrite",
+    "application/clarisworks",
+    "application/macwriteii",
+    "application/x-abiword",
+    "application/x-t602",
+    "application/vnd.lotus-wordpro",
+    "text/plain",
+    "application/x-hwp",
+    "application/vnd.sun.xml.writer.template",
+    "application/pdf",
+    "application/vnd.oasis.opendocument.text",
+    "application/vnd.oasis.opendocument.text-template",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.slideshow",
+    "application/x-fictionbook+xml",
+    "application/x-aportisdoc",
+    "application/prs.plucker",
+    "application/x-iwork-pages-sffpages",
+    "application/vnd.palm",
+    "application/epub+zip",
+    "application/x-pocket-word",
+    "application/vnd.oasis.opendocument.spreadsheet-flat-xml",
+    "application/vnd.lotus-1-2-3",
+    "application/vnd.ms-excel",
+    "text/spreadsheet",
+    "application/vnd.sun.xml.calc",
+    "application/vnd.sun.xml.calc.template",
+    "application/x-gnumeric",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.ms-excel.sheet.macroEnabled.12",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.template",
+    "application/clarisworks",
+    "application/x-iwork-numbers-sffnumbers",
+    "application/mathml+xml",
+    "application/vnd.sun.xml.math",
+    "application/vnd.oasis.opendocument.formula",
+    "application/vnd.sun.xml.base",
+    "image/jpeg",
+    "image/png",
+    "image/svg+xml",
+    "image/webp",
+    "application/docbook+xml",
+    "application/xhtml+xml",
+];
